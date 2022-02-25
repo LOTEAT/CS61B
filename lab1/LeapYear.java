73a2f9b03291c4a1e0fafa305442b9bd07aeb800
LOTEAT <zenglezhu@gmail.com>
2022-02-25 19:31:34 +0800

    lab 1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,7 +1,20 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author LOTEAT
  */
 public class LeapYear {
+
+    /**
+     *
+     * @param year year
+     * @return whether this year is a leap year
+     */
+    public static boolean isLeapYear(int year){
+        if(year % 400 == 0)
+            return true;
+        else if(year % 4 == 0 && year % 100 != 0)
+            return true;
+        return false;
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
