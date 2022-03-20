c0c7d6f0e8ee9efd1d77c3084a34c5acf19fc94a
wendioooo <yu.wendi@outlook.com>
2022-03-20 21:36:02 +0800

    update

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -14,50 +14,49 @@ public class LinkedListDeque<T> {
         public DequeNode next;
 
         public DequeNode(){
-            prev=null;
-            next=null;
+            prev = null;
+            next = null;
         }
         public DequeNode(T x){
-            item=x;
-            prev=null;
-            next=null;
+            item = x;
+            prev = null;
+            next = null;
         }
     }
 
     public LinkedListDeque(){
-        size=0;
-        first=new DequeNode();
-        last=new DequeNode();
-        first.next=last;
-        last.prev=first;
+        size = 0;
+        first = new DequeNode();
+        last = new DequeNode();
+        first.next = last;
+        last.prev = first;
     }
 
 
 
     public void addFirst(T item){
-
-        size+=1;
-        DequeNode cur=new DequeNode(item);
-        DequeNode temp=first.next;
-        first.next=cur;
-        cur.next=temp;
-        cur.prev=first;
-        temp.prev=cur;
+        size += 1;
+        DequeNode cur = new DequeNode(item);
+        DequeNode temp = first.next;
+        first.next = cur;
+        cur.next = temp;
+        cur.prev = first;
+        temp.prev = cur;
 
     }
 
     public void addLast(T item){
-        size+=1;
-        DequeNode temp=last.prev;
-        DequeNode cur=new DequeNode(item);
-        cur.next=last;
-        cur.prev=temp;
-        temp.next=cur;
-        last.prev=cur;
+        size += 1;
+        DequeNode temp = last.prev;
+        DequeNode cur = new DequeNode(item);
+        cur.next = last;
+        cur.prev = temp;
+        temp.next = cur;
+        last.prev = cur;
     }
 
     public boolean isEmpty(){
-        return (size==0);
+        return (size == 0);
     }
 
     public int size(){
@@ -65,38 +64,38 @@ public class LinkedListDeque<T> {
     }
 
     public void printDeque(){
-        DequeNode p=first.next;
-        while(p.next!=null){
-            System.out.println(p.item+" ");
-            p=p.next;
+        DequeNode p = first.next;
+        while(p.next != null){
+            System.out.println(p.item + " ");
+            p = p.next;
         }
     }
 
     public T removeFirst(){
-        if(first.next==last)return null;
-        size-=1;
-        DequeNode rmv=first.next;
-        DequeNode temp=rmv.next;
-        first.next=temp;
-        temp.prev=first;
+        if(first.next == last)return null;
+        size -= 1;
+        DequeNode rmv = first.next;
+        DequeNode temp = rmv.next;
+        first.next = temp;
+        temp.prev = first;
         return rmv.item;
     }
 
 
     public T removeLast(){
-        if(last.prev==first)return null;
-        size-=1;
-        DequeNode rmv=last.prev;
-        DequeNode temp=rmv.prev;
-        temp.next=last;
-        last.prev=temp;
+        if(last.prev == first)return null;
+        size -= 1;
+        DequeNode rmv = last.prev;
+        DequeNode temp = rmv.prev;
+        temp.next = last;
+        last.prev = temp;
         return rmv.item;
     }
 
     public T get(int index){
-        DequeNode p=first;
-        for(int i=0;i<index;++i){
-            p=p.next;
+        DequeNode p = first;
+        for(int i = 0;i < index;++i){
+            p = p.next;
         }
         return p.item;
     }
