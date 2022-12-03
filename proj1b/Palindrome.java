323501846542990f32e5a6cd34382b04e6fb4345
LOTEAT <zenglezhu@gmail.com>
2022-12-03 23:40:53 +0800

    task 3a

---
+++ proj1b/Palindrome.java
@@ -0,0 +1,21 @@
+public class Palindrome {
+    public Deque<Character> wordToDeque(String word){
+        Deque<Character> word_deque = new ArrayDeque<>();
+        int len = word.length();
+        for (int i = 0; i < len; i++) {
+            word_deque.addLast(word.charAt(i));
+        }
+        return word_deque;
+    }
+    public boolean isPalindrome(String word){
+        int start = 0, end = word.length() - 1;
+        while(start < end){
+            if(word.charAt(start) != word.charAt(end))
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+}
